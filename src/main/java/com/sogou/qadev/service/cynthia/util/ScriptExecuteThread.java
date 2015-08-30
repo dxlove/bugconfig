0278929bf105ca8e41ba33ca014b6f1fb4d8e1f9
dxlove <duxu111@gmail.com>
2015-08-30 10:36:00 +0800

    初始化

---
+++ src/main/java/com/sogou/qadev/service/cynthia/util/ScriptExecuteThread.java
@@ -0,0 +1,50 @@
+package com.sogou.qadev.service.cynthia.util;
+
+import org.jfree.util.Log;
+
+import bsh.EvalError;
+import bsh.Interpreter;
+
+import com.sogou.qadev.service.cynthia.bean.Script;
+import com.sogou.qadev.service.cynthia.service.ScriptExecuteManager;
+
+public class ScriptExecuteThread implements Runnable {
+
+	public void run() {
+		while (true) {
+			Interpreter interpreter = null;
+			// 提取值，需要锁住对象
+			synchronized (ScriptExecuteManager.allDeamonScript) {
+				if (ScriptExecuteManager.allDeamonScript.size() == 0) {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					continue;
+				}
+				interpreter = ScriptExecuteManager.allDeamonScript.remove(0);
+			}
+
+			if (interpreter != null) {
+				try {
+					Script script = (Script) interpreter.get("script");
+					if (script == null)
+						continue;
+
+					String content = script.getScript();
+					if (content == null)
+						continue;
+					//System.err.println("content:"+content);
+					interpreter.eval(content);
+				} catch (EvalError e) {
+					e.printStackTrace();
+					Log.error(e.getMessage(), e);
+				}
+			}
+
+		}
+
+	}
+
+}
